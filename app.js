55de8a4d14a17e71573a525f0304b69e3e5916bc
Twinkle Devwanshi <twinkledevwanshi93@gmail.com>
2025-07-05 15:26:37 -0700

    updated the msg

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Node.js!\n');
+  res.end('My name is Twinkle Devwanshi \n');
 });
 
 server.listen(port, () => {
